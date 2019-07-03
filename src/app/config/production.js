37f04755bcc3f38ba109123e0a5afedf939ac39f
Markus Siebeneicher <siebeneicher@mardal.biz>
2019-07-03 01:07:04 +0100

    prod config

--- src/app/config/production.js
+++ src/app/config/production.js
@@ -5,4 +5,19 @@ module.exports = {
 		host: 'localhost',
 		port: 80
 	},
+	mongo: {
+		url: 'mongodb://localhost:27017',
+		options: {
+			useNewUrlParser: true,
+			//https://stackoverflow.com/questions/30909492/mongoerror-topology-was-destroyed
+			keepAlive: 1,
+			connectTimeoutMS: 30000,
+			reconnectTries: Number.MAX_VALUE,
+			reconnectInterval: 1000
+		},
+		db: {
+			sync: 'cpc_sync',
+			aggregation: 'cpc_aggregations'
+		}
+	},
 };
