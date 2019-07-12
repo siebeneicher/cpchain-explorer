15883991986890d54c61f9e1cb87364c16c3ba1b
Markus Siebeneicher <siebeneicher@mardal.biz>
2019-07-12 17:14:33 +0100

    delayed backwards sync

--- src/sync.js
+++ src/sync.js
@@ -15,7 +15,7 @@ let last_blockNumber = 0;			// most recent block number
 let last_blockNumber_synced = 0;	// most recent synced
 
 const sync_delay = 500;
-const backwards_delay = 5000;
+const backwards_delay = 25000;
 const balance_delay = 10000;
 const maxNewBlocksBackwardsPerCycle = 15;
 
