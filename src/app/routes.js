1d0586ec9d0921d801a87dd5410cf90bc584dfde
Markus Siebeneicher <siebeneicher@mardal.biz>
2019-08-07 20:13:12 +0200

    test

--- src/app/routes.js
+++ src/app/routes.js
@@ -126,12 +126,11 @@ app.use('/', express.static(__dirname + '/../ui-build'));
 
 app.get(/^\/(blocks|block|transaction|transactions|trx|txs|tx|address|rnode|rich-list|system-status|stats|rnodes)/, async function (req, res) {
 	res.setHeader('X-Used-Frontend-Cache', 'no');
-	console.log("SERVING HTML");
+/*	console.log("SERVING HTML");
 	console.log(__dirname + '/../MAINTENANCE');
 
 	res.sendFile(path.join(__dirname + '/maintenance.html'));
-	return;
-
+	return;*/
 	if (await maintenance())
 		res.sendFile(path.join(__dirname + '/maintenance.html'));
 	else
