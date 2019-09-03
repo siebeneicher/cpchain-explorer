ab7771b3b749d96f30d78d025db1b4f697a3d0b1
Markus Siebeneicher <siebeneicher@mardal.biz>
2019-09-03 19:45:04 +0200

    pm2 inspect ports

--- src/pm2.config.js
+++ src/pm2.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps : [{
     name: "sync",
     script: "./sync.js",
+    node_args: ["--inspect=9301"],
     args: "",
-    node_args: [/*"--inspect","--inspect-port=9229"*/],
     instances: 1,
     exec_mode: "cluster",
     max_memory_restart: "400M",
@@ -21,6 +21,7 @@ module.exports = {
   }, {
     name: "aggregate",
     script: "./aggregate.js",
+    node_args: ["--inspect=9302"],
     args: "",
     instances: 1,
     exec_mode: "cluster",
@@ -39,6 +40,7 @@ module.exports = {
   }, {
     name: "worker",
     script: "./worker.js",
+    node_args: ["--inspect=9300"],
     args: "",
     instances: 1,
     exec_mode: "cluster",
